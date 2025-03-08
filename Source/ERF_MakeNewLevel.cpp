a9e6ec149e3379c07986981d695fa9506c96c2bd
Ann Almgren <asalmgren@lbl.gov>
2025-03-07 16:19:28 -0800

    fix when we initialize forest drag on restart (#2174)

--- Source/ERF_MakeNewLevel.cpp
+++ Source/ERF_MakeNewLevel.cpp
@@ -166,8 +166,10 @@ void ERF::MakeNewLevelFromScratch (int lev, Real time, const BoxArray& ba_in,
     // ********************************************************************************************
     // Build the data structures for canopy model (depends upon z_phys)
     // ********************************************************************************************
-    if (solverChoice.do_forest_drag) {
-        m_forest_drag[lev]->define_drag_field(ba, dm, geom[lev], z_phys_nd[lev].get());
+    if (restart_chkfile.empty()) {
+        if (solverChoice.do_forest_drag) {
+            m_forest_drag[lev]->define_drag_field(ba, dm, geom[lev], z_phys_cc[lev].get());
+        }
     }
 
     //********************************************************************************************
@@ -266,7 +268,7 @@ ERF::MakeNewLevelFromCoarse (int lev, Real time, const BoxArray& ba,
     // Build the data structures for canopy model (depends upon z_phys)
     // ********************************************************************************************
     if (solverChoice.do_forest_drag) {
-        m_forest_drag[lev]->define_drag_field(ba, dm, geom[lev], z_phys_nd[lev].get());
+        m_forest_drag[lev]->define_drag_field(ba, dm, geom[lev], z_phys_cc[lev].get());
     }
 
     //********************************************************************************************
@@ -399,7 +401,7 @@ ERF::RemakeLevel (int lev, Real time, const BoxArray& ba, const DistributionMapp
     // Build the data structures for canopy model (depends upon z_phys)
     // ********************************************************************************************
     if (solverChoice.do_forest_drag) {
-        m_forest_drag[lev]->define_drag_field(ba, dm, geom[lev], z_phys_nd[lev].get());
+        m_forest_drag[lev]->define_drag_field(ba, dm, geom[lev], z_phys_cc[lev].get());
     }
 
     // *****************************************************************************************************
