14b22b1cacefb55f98414270e8ccebb989d12440
Kyle <kyle.pressel@pnnl.gov>
2026-05-12 17:38:29 -0700

    Address unit test review comments

--- Tests/Unit/ERF_GTestRuntimeStubs.cpp
+++ Tests/Unit/ERF_GTestRuntimeStubs.cpp
@@ -1,3 +1,5 @@
 #include <string>
 
+// The unit-test binary reuses ERF runtime objects that reference the global
+// inputs_name symbol from Source/main.cpp via ERF_WriteJobInfo.cpp.
 std::string inputs_name;
